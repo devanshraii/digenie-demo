c298367c7c42202530683edf9fc2dfbc653b4337
Devansh Rai <devanshrai96@gmail.com>
2024-06-01 08:35:08 +0530

    final

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Card = ({ title, description }) => {
   return (
     <div className="bg-white p-6 rounded-lg shadow-lg transform hover:bg-blue-500 transition duration-300 ease-in-out group">
-      <h3 className="text-2xl font-bold mb-2 group-hover:text-white">{title}</h3>
+      <h3 className=" text-gray-700text-2xl font-bold mb-2 group-hover:text-white">{title}</h3>
       <p className="text-gray-700 group-hover:text-white">{description}</p>
       <div className="text-right mt-4">
         <span className="text-blue-500 group-hover:text-white">&rarr;</span>
